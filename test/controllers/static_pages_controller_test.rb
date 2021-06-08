0dc01d3aea210a75937d120e0a3db496e15cd0f2
vagrant <vagrant@localhost.localdomain>
2021-06-08 21:25:24 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,5 +4,6 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Team_Care"
   end
 end
