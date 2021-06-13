07fe4c4c573f373c17023c33ffcc3fd2d98f597a
kawarazaki3782 <kawarazaki3782@gmail.com>
2021-06-13 17:34:11 +0000

    Profile edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
@@ -25,10 +27,45 @@ class UsersController < ApplicationController
       render 'new'
     end
     end
-  end  
+  end 
+  
+  def edit
+  end
+
+  def verification
+    @user = User.find(params[:id])
+    render 'edit' if @user.invalid?
+  end 
+  
+  def update
+    if params[:back]
+      render 'edit' 
+    elsif @user.update(user_params)
+      # 更新に成功した場合を扱う
+       flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+    end
 
 private
     
   def user_params
     params.require(:user).permit(:name, :email, :profile_image, :password, :password_confirmation, :gender, :birthday, :address, :long_teamcare, :profile, :profile_image_cache)
+  end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
   post '/signup' => 'users#confirm'
+  post '/edit_user_pathuser' => 'users#verification'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
