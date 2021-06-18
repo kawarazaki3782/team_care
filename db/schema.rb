7f46d612817009ec9b8825674f2239f2fa8593d2
kawarazaki3782 <kawarazaki3782@gmail.com>
2021-06-18 20:56:22 +0000

    add microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_114243) do
+ActiveRecord::Schema.define(version: 2021_06_18_205252) do
+
+  create_table "microposts", charset: "utf8mb3", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "relationships", charset: "utf8mb3", force: :cascade do |t|
     t.integer "follower_id"
@@ -39,4 +48,5 @@ ActiveRecord::Schema.define(version: 2021_06_17_114243) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
