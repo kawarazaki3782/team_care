75529e9a27f97c0777a1025c40afb6953d84db36
kawarazaki3782 <kawarazaki3782@gmail.com>
2021-07-27 01:21:27 +0000

    ハンバーガーメニュー訂正

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,6 +1,8 @@
+document.addEventListener("turbolinks:load", function() {
 $(function() {
 	$('.btn-gNav').on("click", function(){
 		$(this).toggleClass('open');
 		$('#gNav').toggleClass('open');
 	});
+});
 });
