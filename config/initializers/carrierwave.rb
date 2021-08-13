e43c7791352c9d8a2282b6349ab39e9a60084ce7
kawarazaki3782 <kawarazaki3782@gmail.com>
2021-08-13 09:22:23 +0000

    S３訂正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,22 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: 'AKIAZMSM2OLSXWTY4VGF',
-        aws_secret_access_key: '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',
-        region: 'ap-northeast-1'
-      }
-    
-      config.fog_directory  = 'teamcare-images'
-      config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/teamcare-images"
-      # config.fog_public = false
-      config.cache_storage = :fog
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  if Rails.env.production? 
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory  = 'teamcare-images' 
+    config.fog_public = false
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], 
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], 
+      region: 'ap-northeast-1', 
+      path_style: true
+    }
+  else 
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
