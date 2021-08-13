29ceef29e85b61eb7cea12b756c260c459749d07
kawarazaki3782 <kawarazaki3782@gmail.com>
2021-08-13 10:31:52 +0000

    お問い合わせ設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,7 +101,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "teamcare.herokuapp.com"
+  host = "#{ENV['HEROKU_APPNAME']}.herokuapp.com"
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
