22a0adac258170e0dadca551408010f50a43c2eb
ohtsuka takahiro <t.o.0526@gmail.com>
2021-08-26 08:43:47 +0900

    has_manyなど並び替え

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,19 +1,22 @@
 class Diary < ApplicationRecord
-  belongs_to :user
   default_scope -> { order(created_at: :desc) }
+
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 5000 }
+  validates :status, inclusion: { in: Diary.statuses.keys }
+
+  belongs_to :user
   belongs_to :category, optional: true
   has_many :comments, dependent: :destroy
-  mount_uploader :diary_image, DiaryImageUploader
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
-  enum status: { draft: 0, published: 1 }
   has_many :favorites
   has_many :users, through: :favorites
-  validates :status, inclusion: { in: Diary.statuses.keys }
   has_many :notifications, dependent: :destroy
+
+  mount_uploader :diary_image, DiaryImageUploader
+  enum status: { draft: 0, published: 1 }
 
   def create_notification_by(current_user)
     current_user.active_notifications.create(
