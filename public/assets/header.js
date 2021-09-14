169df333858fbfb293a8c669d8cc2a6f0e0b5e29
kawarazaki3782 <kawarazaki3782@gmail.com>
2021-09-14 05:50:38 +0000

    header.js再訂正

--- public/assets/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("turbolinks:load", function() {
-$(function() {
-	$('.btn-gNav').on("click", function(){
-		$(this).toggleClass('open');
-		$('#gNav').toggleClass('open');
-	});
-});
-});
